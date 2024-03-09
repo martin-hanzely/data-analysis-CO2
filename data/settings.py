cab63efc1700e82d5c3a0d929381edb1f9fddd7d
Martin Hanzely <hanzely.martin@outlook.com>
2024-03-09 20:01:36 +0100

    Added Celery dep

--- data/settings.py
+++ data/settings.py
@@ -8,3 +8,7 @@ class Settings(BaseSettings):
     earthdata_base_url: str
     earthdata_username: str
     earthdata_password: str
+
+    # CELERY
+    celery_broker_url: str
+    celery_result_backend: str
