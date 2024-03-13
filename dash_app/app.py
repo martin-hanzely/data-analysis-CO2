d0a683654a4bc51f07153bd7cabfee2444dc7644
Martin Hanzely <hanzely.martin@outlook.com>
2024-03-13 23:21:26 +0100

    Updated app UI

--- dash_app/app.py
+++ dash_app/app.py
@@ -34,56 +34,71 @@ external_stylesheets = [{
     "crossorigin": "anonymous"
 }]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(
+    __name__,
+    external_stylesheets=external_stylesheets,
+)
 
 server = app.server
 
 loader = InfluxDBLoader(settings=settings)
 
+base_date = dt.date(2024, 1, 1)
+date_range = 7
+
+# Load `df` in global state
+logger.info("Loading data from InfluxDB")
+df = loader.retrieve_dataframe(
+    dt_from=pd.Timestamp(base_date, tz="UTC"),
+    dt_to=pd.Timestamp(base_date + dt.timedelta(date_range), tz="UTC"),
+)
+logger.info("Data loaded")
+
+# TODO: Move this to ETL pipeline!
+logger.info("Performing aggregation")
+df["latitude"] = df["latitude"].apply(lambda x: round(x))
+df["longitude"] = df["longitude"].apply(lambda x: round(x))
+df["date"] = df["_time"].apply(lambda x: x.date())
+df = df.groupby(["latitude", "longitude", "date"]).mean(numeric_only=True).reset_index()
+
+xco2_min = df["xco2"].min()
+xco2_max = df["xco2"].max()
+
 app.layout = dash.html.Div([
     dash.html.H1(children="Data analysis CO2", style={"textAlign": "center"}),
     dash.html.Div([
-        dash.html.Label("Input"),
-        dash.dcc.DatePickerSingle(
-            id="date-picker",
-            min_date_allowed=dt.date(2024, 1, 1),
-            max_date_allowed=dt.date(2024, 1, 31),
-            initial_visible_month=dt.date(2024, 1, 1),
-            date=dt.date(2024, 1, 1),
-            display_format="YYYY-MM-DD",
-        )
-    ]),
-    dash.html.Div([
         dash.dcc.Graph(id="graph-content"),
+        dash.dcc.RangeSlider(
+            min=0,
+            max=date_range,
+            step=1,
+            value=[0, date_range],
+            id="range-slider",
+        ),
     ])
 ])
 
 
 @dash.callback(
     dash.Output(component_id="graph-content", component_property="figure"),
-    dash.Input(component_id="date-picker", component_property="date")
+    dash.Input(component_id="range-slider", component_property="value"),
 )
-def update_output_div(date: str) -> Figure:
-    _d = dt.datetime.strptime(date, "%Y-%m-%d")
-    logger.info("Input value: %s", _d)
-    df = loader.retrieve_dataframe(
-        dt_from=pd.Timestamp(_d, tz="UTC"),
-        dt_to=pd.Timestamp(_d, tz="UTC") + pd.Timedelta(days=1),
-    )
+def update_output_div(value: list[int]) -> Figure:
+    try:
+        date_from = base_date + dt.timedelta(value[0])
+        date_to = base_date + dt.timedelta(value[1])
+    except (TypeError, IndexError) as e:
+        logger.error("Invalid date range", exc_info=e)
+        raise dash.exceptions.PreventUpdate
 
-    df["date"] = df["_time"].apply(lambda x: x.date())
-
-    # Aggregate data per degree
-    df["latitude"] = df["latitude"].apply(lambda x: round(x))
-    df["longitude"] = df["longitude"].apply(lambda x: round(x))
-    df = df.groupby(["latitude", "longitude", "date"]).mean(numeric_only=True).reset_index()
+    _df = df[df["date"].between(date_from, date_to)]
 
     fig = px.scatter_geo(
-        df,
+        _df,
         lat="latitude",
         lon="longitude",
         color="xco2",
         hover_data=["xco2"],
-        animation_frame="date",
+        range_color=[xco2_min, xco2_max],
     )
     return fig
