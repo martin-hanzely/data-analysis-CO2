98df593b6da2bff5ba3d1a746b9c628839355715
Martin Hanzely <hanzely.martin@outlook.com>
2024-05-01 00:20:33 +0200

    Working on new UI

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import time
 import datetime as dt
 import logging
 import uuid
@@ -12,12 +11,10 @@ import plotly.express as px
 import sentry_sdk
 
 from data.conf import get_app_settings
-from data.loaders.influxdb_loader import InfluxDBLoader
+from data.loaders.local_parquet_loader import LocalParquetLoader
 
 if TYPE_CHECKING:
     from plotly.graph_objs import Figure
-
-    from data.loaders.base_loader import BaseLoader
 
 
 class CallbackManagerCacheSettings(TypedDict):
@@ -66,106 +63,123 @@ app = dash.Dash(
 
 server = app.server
 
-_min_date = "2024-01-01"
 _today = dt.date.today()
+
+loader = LocalParquetLoader()
+# loader = S3ParquetLoader(settings=settings)
+
+df = loader.retrieve_dataframe("out_/oco2_daily_avg.gzip")
+df["date"] = pd.to_datetime(df["date"], errors="coerce")
+# TODO: Pass datetime columns to the loader and let it handle the conversion.
+
+# FIGURE 1
+df["normalized_date"] = pd.to_datetime(df["date"].dt.strftime("%d-%b-1900"), errors="coerce")
+df["year"] = df["date"].dt.year
+
+fig1 = px \
+    .line(df, x="normalized_date", y="average", color="year") \
+    .update_layout(xaxis={"dtick": "M1", "tickformat": "%d-%b"})
+
+# FIGURE 2
+fig2 = px.line(df, x="date", y="average", title="OCO-2")
+fig2.update_xaxes(rangeslider_visible=True)
+
+# FIGURE 3
+fig3 = px.scatter(df, x="date", y=df.columns[1:4], title="OCO-2")
+
+# FIGURE 4
+df_mlo = loader.retrieve_dataframe("out_/mlo.gzip")
+df_mlo["date"] = pd.to_datetime(df_mlo["date"], errors="coerce")
+
+fig4 = px.line(df_mlo, x="date", y="value", title="MLO")
+
+# FIGURE 5 SLIDER
+df_avg_per_month_lat_lon = loader.retrieve_dataframe("out_/monthly_avg_per_lat_lon.gzip")
 
 app.layout = dash.html.Div(
     className="container-fluid",
     children=[
+        dash.html.H1("Bakalárska práca", className="h3"),
+        dash.html.P("Analýza a vizualizácia obsahu oxidu uhličitého v zemskej atmosfére na základe verejne dostupných dát v súvislosti so zmenami klímy."),
         dash.html.Div(
-            className="row",
+            className="row row-cols-2",
             children=[
-                dash.html.Div(
-                    className="col-3 p-5 bg-body-tertiary border-end",
-                    children=[
-                        dash.html.H1("Bakalárska práca", className="h3"),
-                        dash.html.P("Analýza a vizualizácia obsahu oxidu uhličitého v zemskej atmosfére na základe verejne dostupných dát v súvislosti so zmenami klímy."),
-                        dash.html.P("Rozsah zobrazených záznamov: "),
-                        dash.dcc.DatePickerRange(
-                            id='date-picker-range',
-                            start_date=_today - dt.timedelta(days=7),
-                            end_date=_today,
-                            min_date_allowed=_min_date,
-                            max_date_allowed=_today,
-                            initial_visible_month=_today,
-                            display_format="YYYY-MM-DD",
-                            first_day_of_week=1,
-                            updatemode="bothdates",
-                            style={"border-radius": "1rem", "width": "100%"},
-                            className="mb-3",
-                        ),
-                        dash.html.P(["Počet záznamov: ", dash.html.Span(id="results-count")]),
-                        dash.html.P([
-                            "Vypracoval: Martin Hanzely ",
-                            dash.html.A(
-                                href="https://github.com/martin-hanzely/data-analysis-CO2",
-                                children="GitHub",
-                            ),
-                        ]),
-                        dash.html.H5("Zdroje dát"),
-                        dash.html.P([
-                            f"OCO-2/OCO-3 Science Team, Vivienne Payne, Abhishek Chatterjee (2022), OCO-2 Level 2 geolocated XCO2 retrievals results, physical model V11, Greenbelt, MD, USA, Goddard Earth Sciences Data and Information Services Center (GES DISC), Accessed: {_today.isoformat()}, ",
-                            dash.html.A(
-                                href="https://disc.gsfc.nasa.gov/datacollection/OCO2_L2_Standard_11.html",
-                                children="Link",
-                            )
-                        ]),
-                        dash.html.P([
-                            f"OCO-2/OCO-3 Science Team, Vivienne Payne, Abhishek Chatterjee (2022), OCO-2 Level 2 bias-corrected XCO2 and other select fields from the full-physics retrieval aggregated as daily files, Retrospective processing V11.1r, Greenbelt, MD, USA, Goddard Earth Sciences Data and Information Services Center (GES DISC), Accessed: {_today.isoformat()}, ",
-                            dash.html.A(
-                                href="https://doi.org/10.5067/8E4VLCK16O6Q",
-                                children="10.5067/8E4VLCK16O6Q",
-                            )
-                        ]),
-                    ],
-                ),
-                dash.html.Div(
-                    [dash.dcc.Graph(id="graph-content", style={"height": "100vh"})],
-                    className="col-9"
-                )
+                dash.html.Div(dash.dcc.Graph(figure=fig1), className="col"),
+                dash.html.Div(dash.dcc.Graph(figure=fig2), className="col"),
+                dash.html.Div(dash.dcc.Graph(figure=fig3), className="col"),
+                dash.html.Div(dash.dcc.Graph(figure=fig4), className="col"),
             ]
         ),
+        dash.html.Div(
+            children=[
+                dash.dcc.Graph(id="graph-content5", style={"height": "100vh"}),
+                dash.dcc.Slider(
+                    min=2024,
+                    max=2023,
+                    step=1,
+                    value=2023,
+                    marks={2023: "Rok 2023", 2024: "Rok 2024"},
+                    id="year-slider"
+                ),
+                dash.dcc.Slider(
+                    min=1,
+                    max=12,
+                    step=1,
+                    value=1,
+                    marks={1: "1", 12: "12"},
+                    id="month-slider"
+                ),
+            ],
+        ),
+        dash.html.Div(
+            className="py-4",
+            children=[
+                dash.html.H5("Zdroje dát"),
+                dash.html.P([
+                    f"OCO-2/OCO-3 Science Team, Vivienne Payne, Abhishek Chatterjee (2022), OCO-2 Level 2 geolocated XCO2 retrievals results, physical model V11, Greenbelt, MD, USA, Goddard Earth Sciences Data and Information Services Center (GES DISC), Accessed: {_today.isoformat()}, ",
+                    dash.html.A(href="https://disc.gsfc.nasa.gov/datacollection/OCO2_L2_Standard_11.html", children="Link")
+                ]),
+                dash.html.P([
+                    f"OCO-2/OCO-3 Science Team, Vivienne Payne, Abhishek Chatterjee (2022), OCO-2 Level 2 bias-corrected XCO2 and other select fields from the full-physics retrieval aggregated as daily files, Retrospective processing V11.1r, Greenbelt, MD, USA, Goddard Earth Sciences Data and Information Services Center (GES DISC), Accessed: {_today.isoformat()}, ",
+                    dash.html.A(href="https://doi.org/10.5067/8E4VLCK16O6Q", children="10.5067/8E4VLCK16O6Q")
+                ]),
+                dash.html.P([
+                    f"Lan, X., J.W. Mund, A.M. Crotwell, M.J. Crotwell, E. Moglia, M. Madronich, D. Neff and K.W. Thoning (2023), Atmospheric Carbon Dioxide Dry Air Mole Fractions from the NOAA GML Carbon Cycle Cooperative Global Air Sampling Network, 1968-2022, Version: 2023-08-28, Accessed: {_today.isoformat()}, ",
+                    dash.html.A(href="https://doi.org/10.15138/wkgj-f215", children="10.15138/wkgj-f215")
+                ]),
+            ],
+        )
     ]
 )
 
-loader = InfluxDBLoader(settings=settings)
-
 
 @dash.callback(
-    dash.Output(component_id="graph-content", component_property="figure"),
-    dash.Output(component_id="results-count", component_property="children"),
-    dash.Input(component_id="date-picker-range", component_property="start_date"),
-    dash.Input(component_id="date-picker-range", component_property="end_date"),
+    dash.Output(component_id="graph-content5", component_property="figure"),
+    dash.Input(component_id="year-slider", component_property="value"),
+    dash.Input(component_id="month-slider", component_property="value"),
     background=True,
-    running=[(dash.Output(component_id="date-picker-range", component_property="disabled"), True, False)]
+    running=[
+        (dash.Output(component_id="year-slider", component_property="disabled"), True, False),
+        (dash.Output(component_id="month-slider", component_property="disabled"), True, False),
+    ]
 )
 def update_graph(
-        start_date: str,
-        end_date: str,
-        loader_: BaseLoader = loader
-) -> tuple[Figure, int]:
-    try:
-        date_from = dt.date.fromisoformat(start_date)
-        date_to = dt.date.fromisoformat(end_date)
-    except ValueError as e:
-        logger.error("Invalid date range", exc_info=e)
-        raise dash.exceptions.PreventUpdate
-
-    _time = time.time()
-    df = loader_.retrieve_dataframe(
-        dt_from=pd.Timestamp(date_from, tz="UTC"),
-        dt_to=pd.Timestamp(date_to, tz="UTC"),
-    )
-    logger.info("Data loaded in %s seconds", time.time() - _time)
-    logger.info("Data shape: %s", df.shape)
-
-    fig = px.scatter_geo(
-        df,
+        year: int,
+        month: int,
+        # loader_: BaseLoader = loader
+) -> Figure:
+    # 5
+    df_ = df_avg_per_month_lat_lon.loc[
+        (df_avg_per_month_lat_lon["year"] == year) & (df_avg_per_month_lat_lon["month"] == month)
+    ]
+    fig5 = px.scatter_geo(
+        df_,
         lat="latitude",
         lon="longitude",
         color="xco2",
-        hover_data=["xco2", "_time"],
+        hover_data=["xco2"],
     )
-    fig.update_geos(projection_type="orthographic")
+    fig5.update_geos(projection_type="orthographic")
 
-    return fig, df.shape[0]
+    return fig5
+
